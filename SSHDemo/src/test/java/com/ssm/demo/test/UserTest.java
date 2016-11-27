110c82e3718f570e583a647b5603f29e7b933e51
ouym <1308181798@qq.com>
2016-11-27 16:59:02 +0800

    修改测试类，基于注解

--- SSHDemo/src/test/java/com/ssm/demo/test/UserTest.java
+++ SSHDemo/src/test/java/com/ssm/demo/test/UserTest.java
@@ -3,27 +3,34 @@ package com.ssm.demo.test;
 import com.ssm.demo.web.model.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.ssm.demo.web.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * user:ouym
  * date: 2015/7/31
  * time: 11:55
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:conf/spring.xml","classpath:conf/spring-mybatis.xml"})
 public class UserTest {
 
-   // @Autowired
+    @Autowired
     private UserService userService;
 
-    @Before
+    /*@Before
     public void before(){
         @SuppressWarnings("resource")
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:conf/spring.xml"
                 ,"classpath:conf/spring-mybatis.xml"});
         userService = (UserService) context.getBean("userServiceImpl");
-    }
+    }*/
 
     @Test
     public void addUser(){
